44f8966aa8f420642fd83dda5d23e6a187f5d6c4
CCIArsene <arsene.esven@cs2i-lorient.fr>
2024-04-04 16:19:40 +0200

    commit

--- app/index.php
+++ app/index.php
@@ -1,34 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HTML Base Template</title>
 
-    <!-- CSS -->
-    <link rel="stylesheet" type="text/css" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="vendor/twbs/bootstrap-icons/font/bootstrap-icons.css">
-    <link rel="stylesheet" type="text/css" href="css/styles.css">
+<head>
+    <?php include "includes/head.php" ?> 
 </head>
 
 <body>
-    <div class="container p-3">
-       <h1><i class="bi bi-tools"></i> Under construction</h1>
+    <?php include "includes/navigation.php" ?> 
 
-        <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Suscipit distinctio, pariatur facilis nesciunt rem natus. 
-            Aut eligendi sequi iure quisquam cumque quibusdam tenetur vel, deleniti recusandae quae fuga, 
-            ratione ea adipisci praesentium. 
-            Quod earum asperiores error, voluptate accusantium neque repudiandae quaerat. 
-            Quisquam, voluptatibus aliquam laboriosam corrupti sed quibusdam tenetur quo esse maiores minus, 
-            consequatur quidem eveniet ! 
-        </p>
+    <div class="container-fluid">
+    <h1 class="display-4" class="text-center"> Bienvenue chez Computerz ! </h1>
     </div>
 
-    <!-- JS -->
+    <div class="container-fluid">
+    <img src="images/promo.jpg" alt="image_promo"> 
+    </div>
+        <div class="row">
+            <div class="col-sm-2">
+                <div>
+                    <h2> <i class="bi-bi-house-fill"></i> test </h2> 
+                    <p> test </p>
+                <div>
+                
+                <div>
+                    <h2> <i class="bi-bi-house-fill"></i> test </h2> 
+                    <p> test </p>
+                </div>
+            </div>
+        <div>
     <script src="vendor/twbs/bootstrap/dist/js/bootstrap.bundle.js"></script>
     <script src="js/scripts.js"></script>
+
+    <?php include "includes/footer.php" ?> 
 </body>
 </html>
